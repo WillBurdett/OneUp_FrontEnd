7ea16e682aff5972d09e8afa9c03e55d7df590ea
Rachel Kaur <rachel.km@outlook.com>
2022-03-24 20:49:31 +0000

    amend  nav bar and log-in button styling

--- src/container/NavBar.js
+++ src/container/NavBar.js
@@ -6,15 +6,16 @@ const Navbar = () => {
 
     return (
 
-    <>
-        <ul id="navbar">
-            <li className="navitem"><img src = {bookLogo} alt = "Book logo"/></li>
+    <div id="navbar">
+        <img className="nav-logo" src = {bookLogo} alt = "Book logo"/>
+        <ul id="navbar-list">
+            {/* <li className="navitem"><img src = {bookLogo} alt = "Book logo"/></li> */}
             <li className="navitem"><Link to="/">Home</Link></li>
             {/* Add nav item for About Us (and corresponding page) */}
             <li className="navitem"><Link to="/">About us</Link></li>
             <li className="navitem"><Link to="/">Contact</Link></li>
         </ul>
-    </>
+    </div>
 
     )
 
