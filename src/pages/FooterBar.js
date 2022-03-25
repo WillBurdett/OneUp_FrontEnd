80f19f64cb67b891d7dd4d8f0f36543d7b686291
Michael Atayero <michaelak1395@gmail.com>
2022-03-25 11:03:17 +0000

    Footer Change

--- src/pages/FooterBar.js
+++ src/pages/FooterBar.js
@@ -45,10 +45,10 @@ const FooterBar = () => {
                     <div className="footer-col">
                         <h4>Follow us</h4>
                         <div className="social-links">
-                            {/* <a className="social-media-icons" href=""><img className="Twitter" src = {TwitterLogo} alt = "Twitter logo"/></a>
-                            <a className="social-media-icons"  href=""><img className="Facebook" src = {FacebookLogo} alt = "Facebook logo"/></a>
-                            <a className="social-media-icons" href=""><img className="Instagram" src = {InstagramLogo} alt = "Instagram logo"/></a>
-                            <a className="social-media-icons" href=""><img className="Youtube" src = {YoutubeLogo} alt = "Youtube logo"/></a> */}
+                            <a className="social-media-icons" href=""><img className="Twitter" src={TwitterLogo} alt="Twitter logo"/></a>
+                            <a className="social-media-icons" href=""><img className="Instagram" src={InstagramLogo} alt="Instagram logo"/></a>
+                            <a className="social-media-icons"  href=""><img className="Facebook" src={FacebookLogo} alt="Facebook logo"/></a>
+                        
                         </div>
                         <p className="footer-description"> © 2022 The London Library. All rights reserved. Charity No. 312175</p>
 
